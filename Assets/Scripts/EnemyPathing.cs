0094118388f7095b5c18daf73ae815b0116f1379
NaruStark <47173300+NaruStark@users.noreply.github.com>
2019-08-02 16:28:35 +0200

    Update, enemy
    first wave spawns

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    private void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // config param
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float laserSpeed = 10f;
+    [SerializeField] float laserFireingPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+        StartCoroutine(FireContinuously());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    IEnumerator FireContinuously()
+    {
+        WaitForSeconds delay = new WaitForSeconds(laserFireingPeriod);
+
+        while (true)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                GameObject laser = Instantiate(
+                    projectile,
+                    transform.position,
+                    Quaternion.identity) as GameObject;
+
+                laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, laserSpeed);
+
+                yield return delay;
+            }
+
+            yield return null; // come back in the next frame
+        }
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newXpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYpos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXpos, newYpos);
+    }
+
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+}
