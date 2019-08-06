22c2a493bbf13453cbefa2d08af32a72eff30adc
NaruStark <47173300+NaruStark@users.noreply.github.com>
2019-08-06 16:56:19 +0200

    Enemy Update
    Basic dmg and health introduced for enemies

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -6,7 +6,6 @@ public class EnemyPathing : MonoBehaviour
 {
     [SerializeField] WaveConfig waveConfig;
     List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
 
     int waypointIndex = 0;
 
@@ -21,12 +20,17 @@ public class EnemyPathing : MonoBehaviour
         Move();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
     private void Move()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPos = waypoints[waypointIndex].position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPos, movementThisFrame);
             if (transform.position == targetPos)
             {
